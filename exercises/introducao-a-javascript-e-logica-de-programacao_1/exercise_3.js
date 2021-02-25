8caac4472d2627284afe16a0676461fc27c99161
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-02-25 13:24:01 -0300

    Exercício 3 - Cria programa que retorna o maior de 3 números

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_3.js
@@ -0,0 +1,11 @@
+let numero1 = 16;
+let numero2 = 11;
+let numero3 = 24;
+
+if (numero1 > numero2 && numero1 > numero3) {
+  console.log('O maior número é: ' + numero1);
+} else if (numero2 > numero1 && numero2 > numero3) {
+  console.log('O maior número é: ' + numero2);
+} else if (numero3 > numero1 && numero3 > numero2) {
+  console.log('O maior número é: ' + numero3);
+}
