5d4eb0376c3a41cae40f73ee04312db14bdc6a3d
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-02-25 13:01:12 -0300

    Exercício 1 - cria programas com operações de aritmética básica

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_1.js
@@ -0,0 +1,22 @@
+let a = 17;
+let b = 28;
+
+// Adição
+let soma = a + b;
+console.log(soma);
+
+// Subtração
+let subtracao = a - b;
+console.log(subtracao);
+
+// Multiplicação
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+// Divisão
+let divisao = a / b;
+console.log(divisao);
+
+// Módulo
+let modulo = b % a;
+console.log(modulo);
