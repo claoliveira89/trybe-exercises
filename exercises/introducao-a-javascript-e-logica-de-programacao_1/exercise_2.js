8488a593ecc62c85686544941b8d58572dab5ba7
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-02-25 13:12:58 -0300

    Exercício 2 - cria programa que retorna o maior de dois números

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_2.js
@@ -0,0 +1,10 @@
+let numero1 = 3;
+let numero2 = 10;
+
+if (numero1 > numero2) {
+  console.log('O maior número é: ' + numero1);
+} else if (numero1 < numero2) {
+  console.log('O maior número é: ' + numero2);
+} else {
+  console.log('Os números são iguais!');
+}
