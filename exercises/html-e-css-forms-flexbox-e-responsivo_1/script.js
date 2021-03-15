d676331d0cbcec6d3e7600ed1a30209710985931
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-15 18:30:45 -0300

    Exercício 1 - Cria fieldset para dados pessoais

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,16 @@
+const selectState = document.getElementById('input-estado');
+const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+function stateOptions(statesList) {
+  let option = document.createElement('option');
+    option.innerText = '--';
+    selectState.appendChild(option);
+  for (state of statesList) {
+    option = document.createElement('option');
+    option.innerText = state;
+    selectState.appendChild(option);
+
+  }
+}
+
+stateOptions(states);
