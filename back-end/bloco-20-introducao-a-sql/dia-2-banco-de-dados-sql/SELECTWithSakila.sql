0470f2adf54da2f8c4bf457c2f5a73d96e134db6
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-13 20:53:20 -0300

    Learning to use SELECT - Exercise 2.2

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECTWithSakila.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECTWithSakila.sql
@@ -1,3 +1,6 @@
 -- No banco sakila:
 -- 2.1) Escreva uma query que selecione todas as colunas da tabela city ;
 SELECT * FROM sakila.city;
+
+-- 2.2) Escreva uma query que exiba apenas as colunas first_name e last_name da tabela customer ;
+SELECT first_name, last_name FROM sakila.customer;
