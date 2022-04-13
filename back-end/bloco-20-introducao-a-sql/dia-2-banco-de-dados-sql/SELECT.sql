1ec0d293685e0904da4b9172cb459834fd11ee6d
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-13 20:30:24 -0300

    Learning to use SELECT - Exercise 1.3

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECT.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECT.sql
@@ -1,5 +1,10 @@
--- Monte uma query que exiba seu nome na tela;
+-- 1.1) Monte uma query que exiba seu nome na tela;
 SELECT 'Clarice';
 
--- Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela;
+-- 1.2) Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela;
 SELECT 'Clarice', 'Oliveira', 'Belo Horizonte', '32';
+
+/* 1.3) Monte uma query que, além de exibir todas as informações já mencionadas,
+        identifique cada coluna usando o AS , que é chamado de alias na linguagem SQL
+        ( alias é como um apelido no português); */
+SELECT 'Clarice' AS nome, 'Oliveira' AS sobrenome, 'Belo Horizonte' AS 'cidade natal', '32' AS idade;
