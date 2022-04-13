d66d10908abf0840ee64ae285c874fbcd1dae208
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-13 20:26:39 -0300

    Learning to use SELECT - Exercise 1.2

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECT.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/SELECT.sql
@@ -1,2 +1,5 @@
 -- Monte uma query que exiba seu nome na tela;
 SELECT 'Clarice';
+
+-- Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela;
+SELECT 'Clarice', 'Oliveira', 'Belo Horizonte', '32';
