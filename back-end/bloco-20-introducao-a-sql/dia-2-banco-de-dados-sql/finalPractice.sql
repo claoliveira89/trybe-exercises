a6e496edbf34d5850ec809fdadb4abeeebf082fd
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 18:45:31 -0300

    Final Practice - Exercise 6

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/finalPractice.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/finalPractice.sql
@@ -12,3 +12,7 @@ SELECT 5 * 4 + 2;
 
 -- 5) Escreva uma query para exibir todas as informações de todos os cientistas.
 SELECT * FROM Scientists.Scientists;
+
+/* 6) Escreva uma query para exibir o nome como "Nome do Projeto" e as horas como
+      "Tempo de Trabalho" de cada projeto. */
+SELECT Name AS "Nome do Projeto", Hours AS "Tempo de Trabalho" FROM Scientists.Projects;
