4d7517b7864811143b1162fd0d599a5e5efeda29
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 17:42:21 -0300

    Practicing all commands - Exercise 1.2)

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
@@ -1,3 +1,6 @@
 -- Para os exercícios a seguir, vamos usar a tabela sakila.film
 -- 1.1) Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
 SELECT title FROM sakila.film;
+
+-- 1.2) Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
+SELECT title, release_year, rating FROM sakila.film;
