487028e7491d4c10d7aed331c29b8ed08639822a
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 15:08:51 -0300

    Learning to use CONCAT - Exercise 1.2

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/CONCAT.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/CONCAT.sql
@@ -1,3 +1,8 @@
 /* 1.1) Na tabela sakila.film , monte uma query que exiba o título e o ano de lançamento dos filmes
         em uma coluna e dê a ela o nome Lançamento do Filme . */
 SELECT CONCAT (title, ' - ', release_year) AS 'Lançamento do Filme' FROM sakila.film;
+
+/* 1.2) Na tabela sakila.film , crie uma query que exiba o título do filme e sua classificação
+        indicativa (PG, G, NC-17) em apenas uma coluna. Dê a ela o nome Classificação .
+        Não se esqueça de deixar um espaço entre as palavras para que fiquem legíveis. */
+SELECT CONCAT (title, ' - ', rating) AS Classificação FROM sakila.film;
