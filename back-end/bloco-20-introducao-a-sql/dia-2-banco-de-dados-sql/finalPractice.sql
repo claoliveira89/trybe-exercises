f950476151ba8e9b5d60d425616e742f50eae931
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 18:35:56 -0300

    Final Practice - Exercise 2

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/finalPractice.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/finalPractice.sql
@@ -1,2 +1,5 @@
 -- 1) Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
 SELECT "This is SQL Exercise, Practice and Solution";
+
+-- 2) Escreva uma query para exibir três números em três colunas.
+SELECT '1', '2', '3';
