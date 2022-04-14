6a0db36a3f687480c3ef54487c81b25b8e1a1bb8
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 16:22:24 -0300

    Learning to use COUNT - Exercise 1.2

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/COUNT.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/COUNT.sql
@@ -2,3 +2,6 @@
 -- 1.1) Quantas senhas temos cadastradas nessa tabela?
 SELECT COUNT(password) FROM sakila.staff;
         -- RESPOSTA: 1
+-- 1.2) Quantas pessoas temos no total trabalhando para nossa empresa?
+SELECT COUNT(active) FROM sakila.staff WHERE  active = '1';
+        -- RESPOSTA: 2
