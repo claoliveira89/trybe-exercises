e9a97df2cb85bac8b672365a02bd26c1941268e5
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 17:45:15 -0300

    Practicing all commands - Exercise 1.3

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
@@ -4,3 +4,7 @@ SELECT title FROM sakila.film;
 
 -- 1.2) Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação.
 SELECT title, release_year, rating FROM sakila.film;
+
+-- 1.3) Quantos filmes temos cadastrados?
+SELECT COUNT(*) FROM sakila.film;
+        -- RESPOSTA: 1000
