cb5e7bcf183337d1352e6843a81a3344f60bc7ae
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-14 18:11:39 -0300

    Practicing all commands - Exercise 2.6

--- back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-banco-de-dados-sql/allTogether.sql
@@ -23,3 +23,12 @@ SELECT * FROM sakila.actor ORDER BY last_name ASC, first_name DESC;
 /* 2.4) Vá até a tabela language do sakila e crie uma pesquisa que mostre os 5 idiomas cadastrados,
         mas não mostre o idioma english . */
 SELECT * FROM sakila.language LIMIT 5 OFFSET 1;
+
+-- 2.5) Vá até a tabela film e selecione todos os dados da tabela.
+SELECT * FROM sakila.film;
+
+/* 2.6) Crie uma query para encontrar os 20 primeiros filmes , incluindo o título , o ano de lançamento,
+        a duração , a classificação indicativa e o custo de substituição . Ordene os resultados pelos
+        filmes com a maior duração e depois pelo menor custo de substituição. */
+SELECT title, release_year, length, rating, replacement_cost FROM sakila.film 
+ORDER BY length ASC, replacement_cost DESC LIMIT 20;
