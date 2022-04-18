7811bf062d95bf3ba81df2bc3c10636a0bf4ae4b
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-18 19:11:21 -0300

    Aggregation functions - Exercise 1.1.4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/funcoes-de-agregacao.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/funcoes-de-agregacao.sql
@@ -8,3 +8,4 @@
 SELECT AVG(length) AS 'Média de Duração' FROM sakila.film;
 SELECT MIN(length) AS 'Duração Mínima' FROM sakila.film;
 SELECT MAX(length) AS 'Duração Máxima' FROM sakila.film;
+SELECT SUM(length) AS 'Tempo de Exibição Total' FROM sakila.film;
