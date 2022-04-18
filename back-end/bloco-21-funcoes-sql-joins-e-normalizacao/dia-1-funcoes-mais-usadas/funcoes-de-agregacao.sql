95e24dbe50df3a5383691f9603525f29197aaa61
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-18 19:15:33 -0300

    Aggregation functions - Exercise 1.1

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/funcoes-de-agregacao.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/funcoes-de-agregacao.sql
@@ -5,8 +5,6 @@
             A soma de todas as durações como 'Tempo de Exibição Total';
         E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes
         Registrados'. */
-SELECT AVG(length) AS 'Média de Duração' FROM sakila.film;
-SELECT MIN(length) AS 'Duração Mínima' FROM sakila.film;
-SELECT MAX(length) AS 'Duração Máxima' FROM sakila.film;
-SELECT SUM(length) AS 'Tempo de Exibição Total' FROM sakila.film;
-SELECT COUNT(length) AS 'Filmes Registrados' FROM sakila.film;
+SELECT AVG(length) AS 'Média de Duração', MIN(length) AS 'Duração Mínima',
+       MAX(length) AS 'Duração Máxima', SUM(length) AS 'Tempo de Exibição Total',
+       COUNT(length) AS 'Filmes Registrados' FROM sakila.film;
