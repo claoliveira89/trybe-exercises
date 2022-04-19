4652d38a58c0ecde5338a5ed2501fd6b9808e28c
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-19 15:22:56 -0300

    GROUP BY - Exercise 1.4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/group-by.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/group-by.sql
@@ -11,3 +11,8 @@ SELECT store_id, active, COUNT(*) FROM sakila.customer GROUP BY store_id, active
         dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela
         classificação indicativa e ordenados da maior média para a menor.*/
 SELECT rating, AVG(length) FROM sakila.film GROUP BY rating ORDER BY AVG(length) DESC;
+
+/* 1.4) Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de
+        endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a
+        menor.*/
+SELECT district, COUNT(*) FROM sakila.address GROUP BY district ORDER BY COUNT(*) DESC;
