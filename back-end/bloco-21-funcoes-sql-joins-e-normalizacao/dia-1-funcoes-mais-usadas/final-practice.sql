457fc10cb651e10fb0e274a6db71c162d8826102
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-19 17:12:24 -0300

    Final practice - Exercise 1.7

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/final-practice.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/final-practice.sql
@@ -30,3 +30,7 @@ FROM hr.employees;
 /* 1.6) Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras
         ( IT_PROG ).*/
 SELECT COUNT(*) FROM hr.employees WHERE JOB_ID = 'IT_PROG';
+
+/* 1.7) Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de
+        cada profissão ( JOB_ID )*/
+SELECT JOB_ID, SUM(SALARY) FROM hr.employees GROUP BY JOB_ID;
