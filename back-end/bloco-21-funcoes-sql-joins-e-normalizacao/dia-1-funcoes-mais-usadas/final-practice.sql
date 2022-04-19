5a71d3c7f7f4dcc0e29c206b5695d46c931ead04
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-19 16:02:00 -0300

    Final practice - Exercice 1.1

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/final-practice.sql
@@ -0,0 +1,4 @@
+/* Para realizar os exercícios propostos para o dia, faremos uso da tabela employees do banco de dados
+   hr. */ 
+-- 1.1) Escreva uma query que exiba o maior salário da tabela.
+SELECT MAX(SALARY) FROM hr.employees;
