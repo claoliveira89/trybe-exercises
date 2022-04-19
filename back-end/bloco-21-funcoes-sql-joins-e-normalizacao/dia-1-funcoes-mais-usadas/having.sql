f9dceb256509d225920ed4742b8967534add8878
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-19 15:39:47 -0300

    HAVING - Exercise 1.2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/having.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/having.sql
@@ -10,3 +10,16 @@ FROM sakila.film
 GROUP BY rating
 HAVING duracao_media > 115.0 AND duracao_media < 121.50
 ORDER BY duracao_media DESC;
+
+/* 1.2) Usando a query a seguir, exiba apenas os valores de total do custo de substituição que estão
+        acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe
+        a query mais legível. Finalize ordenando os resultados de forma crescente.
+            SELECT rating, SUM(replacement_cost)
+            FROM sakila.film
+            GROUP by rating;
+        */
+SELECT rating, SUM(replacement_cost) AS custo_total_substituicao
+FROM sakila.film
+GROUP by rating
+HAVING custo_total_substituicao > 3950.50
+ORDER BY custo_total_substituicao ASC;
