29cc67eebf44193d898b6552bd4b8fea0aec6041
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-19 16:10:42 -0300

    Final practice - Exercise 1.3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/final-practice.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/final-practice.sql
@@ -5,3 +5,10 @@ SELECT MAX(SALARY) FROM hr.employees;
 
 -- 1.2) Escreva uma query que exiba a diferença entre o maior e o menor salário.
 SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
+
+/* 1.3) Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em
+        ordem decrescente.*/
+SELECT JOB_ID, AVG(SALARY) AS media_salarial
+FROM hr.employees
+GROUP BY JOB_ID
+ORDER BY media_salarial DESC;
