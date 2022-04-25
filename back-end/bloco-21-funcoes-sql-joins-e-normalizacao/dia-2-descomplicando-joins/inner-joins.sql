0d02edf9c3777d397ea62c6622f60582a94a24b1
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-04-25 15:10:22 -0300

    Inner Joins - Exercise 1.2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
@@ -4,3 +4,11 @@ SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name), f.film_id
 FROM sakila.actor AS a
 INNER JOIN sakila.film_actor AS f
 ON a.actor_id = f.actor_id;
+
+/* 1.2) Use o JOIN para exibir o nome , sobrenome e endereço de cada um dos funcionários do banco.
+        Use as tabelas staff e address .*/
+SELECT CONCAT(s.first_name, ' ', s.last_name) AS 'Nome Completo', 
+       CONCAT(a.address, ', ', a.district, '. ', a.postal_code) AS Endereço
+FROM sakila.staff AS s
+INNER JOIN sakila.address AS a
+ON s.address_id = a.address_id;
