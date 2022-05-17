4153c62ab73f36c719a67fb0b0df7ac9267ae477
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-05-17 17:47:30 -0300

    Inner Joins - Exercise 1.6

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
@@ -41,3 +41,13 @@ FROM sakila.address AS a
 INNER JOIN sakila.customer AS c
 ON a.address_id = c.address_id
 WHERE c.active = 1 GROUP BY c.customer_id ORDER BY first_name DESC, last_name DESC;
+
+/* 1.6) Monte uma query que exiba o nome, sobrenome e a média de valor (amount) paga aos funcionários
+        no ano de 2006. Use as tabelas payment e staff. Os resultados devem estar agrupados pelo nome
+        e sobrenome do funcionário.*/
+SELECT CONCAT(s.first_name, ' ', s.last_name) AS Nome, AVG(p.amount) AS 'Média de valor'
+FROM sakila.staff AS s
+INNER JOIN sakila.payment AS p
+ON s.staff_id = p.staff_id
+WHERE YEAR(p.payment_date) = 2006
+GROUP BY Nome;
