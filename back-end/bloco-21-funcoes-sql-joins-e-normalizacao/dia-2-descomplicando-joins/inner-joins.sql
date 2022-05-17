40378f4cec327e783afe5521fb06a334fd9832bb
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-05-17 17:36:39 -0300

    Inner Joins - Exercise 1.5

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/inner-joins.sql
@@ -32,3 +32,12 @@ FROM sakila.customer AS c
 INNER JOIN sakila.address AS a
 ON c.address_id = a.address_id
 WHERE a.district = 'California' AND a.first_name LIKE '%rene%';
+
+/* 1.5) Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por
+        nomes de forma decrescente. Exiba somente os clientes ativos. As informações podem ser
+        encontradas na tabela address e customer .*/
+SELECT c.first_name, COUNT(a.address)
+FROM sakila.address AS a
+INNER JOIN sakila.customer AS c
+ON a.address_id = c.address_id
+WHERE c.active = 1 GROUP BY c.customer_id ORDER BY first_name DESC, last_name DESC;
