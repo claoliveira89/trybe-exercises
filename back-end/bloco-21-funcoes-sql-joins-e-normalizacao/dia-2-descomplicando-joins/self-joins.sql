a1d7d442c598d300592bfe1df132d8279092dbf3
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-05-18 17:50:03 -0300

    Self Join - Exercise 1.2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/self-joins.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/self-joins.sql
@@ -7,3 +7,11 @@ FROM hr.employees AS employee
 INNER JOIN hr.employees AS manager
 ON employee.MANAGER_ID = manager.EMPLOYEE_ID
 WHERE employee.DEPARTMENT_ID <> manager.DEPARTMENT_ID;
+
+/* 1.2) Exiba o Nome e a quantidade de pessoas lideradas de cada pessoa gerente.*/
+SELECT CONCAT(manager.FIRST_NAME, ' ', manager.LAST_NAME) AS 'Pessoa Gerente',
+       COUNT(*) AS 'Pessoas Lideradas'
+FROM hr.employees AS manager
+INNER JOIN hr.employees AS employee
+ON employee.MANAGER_ID = manager.EMPLOYEE_ID
+GROUP BY manager.EMPLOYEE_ID;
