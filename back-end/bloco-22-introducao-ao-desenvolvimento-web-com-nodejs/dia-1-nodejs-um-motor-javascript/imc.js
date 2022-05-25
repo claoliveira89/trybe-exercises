19aebf419dfda89593500c5cb76c89f799a0304f
Clarice Oliveira <clarice.oliveira@d20labs.tech>
2022-05-25 19:06:10 -0300

    Um motor JavaScript - Exercício 5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc.js
@@ -1,10 +1,50 @@
 const readline = require('readline-sync');
 
-const peso = readline.questionFloat('Qual seu peso? ');
-let altura = readline.questionInt('Qual sua altura? ');
+function calculaImc(peso, altura) {
+    console.log(`Peso: ${peso}, Altura: ${altura}`);
 
 altura = altura / 100;
 
 const IMC = peso / (altura * altura);
 
-console.log(`O IMC é: ${IMC}`);
+
+if (IMC < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
+  }
+
+  if (IMC >= 18.5 && IMC < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+
+  if (IMC >= 25 && IMC < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+
+  if (IMC >= 30 && IMC < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+
+  if (IMC >= 35 && IMC < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }
+
+  console.log('Situação: Obesidade graus III e IV');
+
+  // return IMC;
+}
+
+function main() {
+    const peso = readline.questionFloat('Qual o seu peso? (em kg)');
+    const altura = readline.questionInt('Qual a sua altura? (em cm)');
+   
+    const imc = calculaImc(peso, altura);
+  
+    // console.log(`IMC: ${imc.toFixed(2)}`);
+  }
+  
+  main();
